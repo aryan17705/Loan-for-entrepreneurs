84f4111b597b4e4f48e0d6aec8bc43a9932de7d4
aryan17705 <aryanpatil.developer@gmail.com>
2026-09-04 21:41:35 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,10 @@
 export default function Footer() {
   return (
     <footer className="nirvaan-footer border-t">
-      <div className="mx-auto w-full max-w-[1440px] px-5 py-8 sm:px-8 lg:px-10">
+      <div className="mx-auto w-full max-w-[1440px] px-5 py-4 sm:px-8 lg:px-10">
 
-        {/* Copyright */}
-        <div className="mt-8 flex flex-col gap-1">
+  {/* Copyright */}
+  <div className="mt-0 flex flex-col gap-1">
           <p className="nirvaan-text-strong text-[14px] font-medium">
             © 2026 All rights reserved.
           </p>
